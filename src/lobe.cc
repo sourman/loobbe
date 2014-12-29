53db0844ff065513a72bbfb8f26ce1eb5973d74b
Ahmed Mansour <ahmed.om@gmail.com>
2014-12-29 13:05:14 -0500

    Add Lobe::connect_randomly implementation
    Start writing implementation for Lobe class methods
    
    Signed-off-by: Ahmed Mansour <ahmed.om@gmail.com>

--- src/lobe.cc
+++ src/lobe.cc
@@ -3,4 +3,20 @@
 
 using namespace std;
 
+bool Lobe::add_x_cells(unsigned long num)
+{
+	int i;
+	for (i = 0; i < num; i++)
+		cell_list_[i] Cell();
+	return false;
+}
 
+bool Lobe::connect_randomly()
+{
+	// TODO: simple expensive implenetation of cell connection
+	// to be replaced in the future with fast intercellular communcation objects
+	int i;
+	unsigned long num_cells = this->num_cells_();
+	for (i = 0; i < num_cells; i++)
+		cell_list_[i]->connect(rand() % num_cells);
+}
