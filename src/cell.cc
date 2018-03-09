09b3688cbe45c08426c0052dce6a26ea30934075
Ahmed Mansour <ahmed.om@gmail.com>
2018-03-09 11:56:14 -0500

    cell.cc: Add implementation file for cell class
    Signed-off-by: Ahmed Mansour <ahmed.om@gmail.com>

---
+++ src/cell.cc
@@ -0,0 +1,9 @@
+bool place_cell(unsigned int ID, struct Position *position)
+{
+	/* Normally we should check that this position is not taken up before
+	 * we take it, but this function needsto be very fast due to the large
+	 * number of cells, so we assume the algorithm doing the placement is
+	 * not evil */
+	this.position = position;
+	return true;
+}
