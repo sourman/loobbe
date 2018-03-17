3ebe316d8b518222d3274ea34fd8a4869da386c6
Ahmed Mansour <ahmed.om@gmail.com>
2018-03-17 15:05:54 -0400

    cell.h: wrap lines at 80 characters
    Signed-off-by: Ahmed Mansour <ahmed.om@gmail.com>

--- src/cell.h
+++ src/cell.h
@@ -14,36 +14,39 @@ class Cell {
 		void poke_me(void);
 
 		// A way to destroy cells in a natural way.
-		// Perhaps the cell gives up its resources, or maybe it releases toxins that
-		// kill surrounding cells before the destructor is called
+		// Perhaps the cell gives up its resources, or maybe it
+		// releases toxins that kill surrounding cells before the
+		// destructor is called
 		void die(Death death_type);
 
 	private:
-		// Depolarize cell consuming surrounding electrolytes as they flood into cell
-		// Trigger a depolarization cascade down axon to target cells
+		// Depolarize cell consuming surrounding electrolytes as they
+		// flood into cell Trigger a depolarization cascade down axon
+		// to target cells
 		void depolarize(void);
 
-		// Glucose concentration in the cell. Glucose is broken by mitochondira to
-		// bind phosphorus with Adenason diphosphate to create adenason triphosphate
-		// commonly known as ATP
+		// Glucose concentration in the cell. Glucose is broken by
+		// mitochondira to bind phosphorus with Adenason diphosphate to
+		// create adenason triphosphate commonly known as ATP
 		unsigned int glucose;
 
-		// ATP concentration in the cell. used by active ion pumps in restoring the
-		// cell to pre-impulse potential (around -60 mV)
+		// ATP concentration in the cell. used by active ion pumps in
+		// restoring the cell to pre-impulse potential (around -60 mV)
 		unsigned ATP;
 
-		// Oxygen concentration in the cell. used by mitochondria in creation of ATP
+		// Oxygen concentration in the cell. used by mitochondria in
+		// creation of ATP
 		unsigned Oxygen;
 
-		// Threshold of triggering this cell, the voltage at which the passive gates
-		// of this cell breakdown and the cell fires
+		// Threshold of triggering this cell, the voltage at which the
+		// passive gates of this cell breakdown and the cell fires
 		unsigned threshold;
 
 		// The voltage potential of this cell relative outside the cell
 		unsigned voltage;
 
-		// Affluence of neibouring cells this determines how effective an impulse
-		// from each neighbor is in triggering this cell
+		// Affluence of neibouring cells this determines how effective
+		// an impulse from each neighbor is in triggering this cell
 		unsigned affluence;
 
 		// Ca2+ concentration in cell
